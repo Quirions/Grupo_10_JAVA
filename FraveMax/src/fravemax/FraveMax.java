83e98fbe68223d3d1bd3e16c04769a5e42857912
jhon1554 <129795045+jhon1554@users.noreply.github.com>
2023-06-10 20:35:45 -0300

    aca estan las pruebas de detalleCompra y DetalleVenta

--- FraveMax/src/fravemax/FraveMax.java
+++ FraveMax/src/fravemax/FraveMax.java
@@ -1,6 +1,11 @@
 
 package fravemax;
 
+import control.DetalleCompraData;
+import control.DetalleVentaData;
+import control.VentaData;
+import entidades.*;
+import java.time.LocalDate;
 import vistas.MenuFravemax;
 
 public class FraveMax {
@@ -8,12 +13,26 @@ public class FraveMax {
     
     public static void main(String[] args) {
         
-        MenuFravemax mf = new MenuFravemax();
-        mf.setVisible(true);
+//        MenuFravemax mf = new MenuFravemax();
+//        mf.setVisible(true);
+        //pruebas DetalleVentaData
+//        Producto producto = new Producto(1, "producto", 500, 10, true);
+//        Cliente cliente = new Cliente(1, "Ariel", "Aricoma", 39675839, "Quines", 266475849, true);
+//        Venta venta = new Venta(1, LocalDate.now(), cliente);
+//        DetalleVenta vd = new DetalleVenta(3, 50, venta, producto);
+//        DetalleVentaData dvd = new DetalleVentaData();
+//        System.out.println(dvd.obtenerUno(7).toString());
+//        System.out.println(dvd.obtenerTodo());
         
-        
-        
-        
+//        dvd.insertar(vd);
+        //pruebas DetalleCompraData
+//        Proveedor proveedor = new Proveedor(1, "razonsocial", "San luis", 266434535);
+//        Compra compra = new Compra(1, proveedor, LocalDate.now());
+//        DetalleCompra dc = new DetalleCompra(1, 4, 1000, compra, producto);
+//        DetalleCompraData dcd = new DetalleCompraData();
+//        dcd.insertar(dc);
+//        System.out.println(dcd.obtenerUno(1));
+//        System.out.println(dcd.obtenerTodo());
     }
     
 }
