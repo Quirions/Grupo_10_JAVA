b027c730db1c171a6097b3471a81a0f78dc55d18
jhon1554 <129795045+jhon1554@users.noreply.github.com>
2023-06-11 19:44:28 -0300

    aca estan las pruebas de detalleCompra y DetalleVenta

--- FraveMax/src/fravemax/FraveMax.java
+++ FraveMax/src/fravemax/FraveMax.java
@@ -6,6 +6,7 @@ import control.DetalleVentaData;
 import control.VentaData;
 import entidades.*;
 import java.time.LocalDate;
+import java.time.Month;
 import vistas.MenuFravemax;
 
 public class FraveMax {
@@ -15,24 +16,38 @@ public class FraveMax {
         
 //        MenuFravemax mf = new MenuFravemax();
 //        mf.setVisible(true);
-        //pruebas DetalleVentaData
-//        Producto producto = new Producto(1, "producto", 500, 10, true);
-//        Cliente cliente = new Cliente(1, "Ariel", "Aricoma", 39675839, "Quines", 266475849, true);
-//        Venta venta = new Venta(1, LocalDate.now(), cliente);
-//        DetalleVenta vd = new DetalleVenta(3, 50, venta, producto);
-//        DetalleVentaData dvd = new DetalleVentaData();
-//        System.out.println(dvd.obtenerUno(7).toString());
-//        System.out.println(dvd.obtenerTodo());
-        
-//        dvd.insertar(vd);
-        //pruebas DetalleCompraData
-//        Proveedor proveedor = new Proveedor(1, "razonsocial", "San luis", 266434535);
-//        Compra compra = new Compra(1, proveedor, LocalDate.now());
-//        DetalleCompra dc = new DetalleCompra(1, 4, 1000, compra, producto);
-//        DetalleCompraData dcd = new DetalleCompraData();
-//        dcd.insertar(dc);
-//        System.out.println(dcd.obtenerUno(1));
-//        System.out.println(dcd.obtenerTodo());
+//******************************************************************************
+        //Pruebas DetalleVentaData
+            //Producto producto = new Producto(1, "producto", 500, 10, true);
+            //Cliente cliente = new Cliente(1, "Ariel", "Aricoma", 39675839, "Quines", 266475849, true);
+            //Venta venta = new Venta(1, LocalDate.now(), cliente);
+            //DetalleVenta vd = new DetalleVenta(3, 50, venta, producto);
+            //DetalleVentaData dvd = new DetalleVentaData();
+            //System.out.println(dvd.obtenerUno(7).toString());
+            //System.out.println(dvd.obtenerTodo());
+           //dvd.insertar(vd);
+//******************************************************************************
+        //Pruebas DetalleCompraData
+            //Producto producto = new Producto(1, "producto", 500, 10, true);
+            //Proveedor proveedor = new Proveedor(1, "razonsocial", "San luis", 266434535);
+            //Compra compra = new Compra(1, proveedor, LocalDate.now());
+            //DetalleCompra dc = new DetalleCompra(1, 4, 1000, compra, producto);
+            //DetalleCompraData dcd = new DetalleCompraData();
+            //dcd.insertar(dc);
+            //System.out.println(dcd.obtenerUno(1));
+            //System.out.println(dcd.obtenerTodo());
+//******************************************************************************
+       //Pruebas VentaData
+            //VentaData vd = new VentaData();
+            //Venta vt = new Venta();
+            //Cliente cliente = new Cliente(1, "Ariel", "Aricoma", 39675839, "Quines", 266475849, true);
+            //Venta venta = new Venta(1, LocalDate.of(2003, 11, 12), cliente);
+            //System.out.println(vd.obtenerUno(1));
+            //System.out.println(vd.obtenerTodo());
+            //vd.insertar(venta);
+            //vd.actualizar(venta);
+            //vd.borrar(1);
+            
     }
     
 }
